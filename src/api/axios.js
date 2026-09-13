efe130bf11a78ab7dd250207b7a54383f4cb144e
Anant Pandey <anantpandey877@gmail.com>
2026-09-13 18:56:38 +0530

    Added frontend CI/CD

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/leavepilot/api",
+    baseURL: "http://54.209.148.200:8080/leavepilot/api",
     headers: {
         "Content-Type": "application/json"
     }
--- src/components/EmployeeNavbar.jsx
+++ src/components/EmployeeNavbar.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-
 function EmployeeNavbar({ employeeName, onLogout }) {
   const navigate = useNavigate();
 
