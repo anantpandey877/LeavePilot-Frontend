ec5b2fb1ef0899732f514fca380cb8ecfe277dbe
Anant Pandey <anantpandey877@gmail.com>
2026-09-18 19:14:26 +0530

    Deployed backend to Kubernetes - fixing bug

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://af12002e62d5c46da958ef6000e62bdd-1696852969.us-east-1.elb.amazonaws.com/leavepilot/api",
+  baseURL: "http://accc505eb12be488583468dea61dfff5-148697153.us-east-1.elb.amazonaws.com/leavepilot/api",
   headers: {
     "Content-Type": "application/json",
   },
