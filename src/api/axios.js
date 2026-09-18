261009f7f1d40964657425f0cff54ee624b8aa3e
Anant Pandey <anantpandey877@gmail.com>
2026-09-18 17:46:30 +0530

    Deployed backend to Kubernetes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://54.209.148.200:8081/leavepilot/api",
+  baseURL: "http://af12002e62d5c46da958ef6000e62bdd-1696852969.us-east-1.elb.amazonaws.com/leavepilot/api",
   headers: {
     "Content-Type": "application/json",
   },
